67b42b8698987b7b48b00e0ba87aceb04aabc6bd
Nikola Stefanov <stefanov.nm@gmail.com>
2016-08-24 12:13:25 +0300

    in process

---
+++ 02.CodeFormating/Events/Events/EventHolder.cs
@@ -0,0 +1,58 @@
+﻿namespace Events
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        MultiDictionary<string, Event> groupByTitle = new MultiDictionary<string, Event>(true);
+        OrderedBag<Event> groupByDate = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+
+            groupByTitle.Add(title.ToLower(), newEvent);
+            groupByDate.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in groupByTitle[title])
+            {
+                removed++;
+                groupByDate.Remove(eventToRemove);
+            }
+
+            groupByTitle.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = groupByDate.RangeFrom(new Event(date, "", ""), true);
+
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
