0cc2b49c4fa79a3cd3087b7edc88a2052bd844ec
Nikola Stefanov <stefanov.nm@gmail.com>
2016-09-13 08:29:00 +0300

    update

--- 04.CodeDocumentationAndComments/CodeDocumentationAndComments/StringExtensions.cs
+++ 04.CodeDocumentationAndComments/CodeDocumentationAndComments/StringExtensions.cs
@@ -26,8 +26,6 @@
             // Convert the input string to a byte array and compute the hash.
             var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
 
-            // Create a new StringBuilder to collect the bytes
-            // and create a string.
             var builder = new StringBuilder();
 
             // Loop through each byte of the hashed data 
@@ -37,7 +35,6 @@
                 builder.Append(data[i].ToString("x2"));
             }
 
-            // Return the hexadecimal string.
             return builder.ToString();
         }
 
